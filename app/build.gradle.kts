d542fc1e6dc32de762c4d6e6ea86b9e29b2ace2b
zivzhao <zivzhao@icloud.com>
2026-03-19 01:10:41 +0800

    update version 1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.tutu.meowhub"
         minSdk = 28
         targetSdk = 28
-        versionCode = 2
-        versionName = "1.2"
+        versionCode = 3
+        versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
